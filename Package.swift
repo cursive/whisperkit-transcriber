0a128d23f4e7a780435dc61d8be331f6cfdd3e93
Daniel Nacamuli <cursive@users.noreply.github.com>
2025-05-09 08:28:30 -0700

    Plugin SWIFT wrapper seems to work

--- Package.swift
+++ Package.swift
@@ -7,22 +7,30 @@ let package = Package(
     products: [
         .library(
             name: "WhisperkitTranscriber",
-            targets: ["WhisperkitTranscriberPlugin"])
+            targets: ["WhisperkitTranscriberPlugin"]
+        )
     ],
     dependencies: [
-        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", from: "7.0.0")
+        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", from: "7.0.0"),
+        .package(url: "https://github.com/argmaxinc/WhisperKit.git", from: "0.9.0"),
     ],
     targets: [
         .target(
             name: "WhisperkitTranscriberPlugin",
             dependencies: [
                 .product(name: "Capacitor", package: "capacitor-swift-pm"),
-                .product(name: "Cordova", package: "capacitor-swift-pm")
+                .product(name: "Cordova", package: "capacitor-swift-pm"),
+                "WhisperKit"
             ],
-            path: "ios/Sources/WhisperkitTranscriberPlugin"),
+            path: "ios/Sources/WhisperkitTranscriberPlugin",
+            resources: [
+                .copy("ggml-base.en.bin")
+            ]
+        ),
         .testTarget(
             name: "WhisperkitTranscriberPluginTests",
             dependencies: ["WhisperkitTranscriberPlugin"],
-            path: "ios/Tests/WhisperkitTranscriberPluginTests")
+            path: "ios/Tests/WhisperkitTranscriberPluginTests"
+        )
     ]
 )
